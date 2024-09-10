00825810a738a534d33847af4bcd17023651cba3
n.ivanov <n.ivanov@proxiad.com>
2024-09-10 18:14:02 +0300

    change the name of 400.jsp to error.jsp

--- src/main/java/controller/GlobalExHandler.java
+++ src/main/java/controller/GlobalExHandler.java
@@ -14,27 +14,27 @@ public class GlobalExHandler {
 
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ModelAndView handleBadRequestException(NoHandlerFoundException ex, Model model) {
-		ModelAndView modelView = new ModelAndView("400");
+		ModelAndView modelView = new ModelAndView("error");
 		modelView.setStatus(HttpStatus.BAD_REQUEST);
-		modelView.addObject("errorMessage", "The URL you are trying to reach is not in service at this time (400 Bad Request :-( ).");
+		modelView.addObject("errorMessage", "The URL you are trying to reach is not in service at this time ( 400 Bad Request :-( ).");
 		
 		return modelView;
 	}
 	
 	@ExceptionHandler(InternalServerError.class)
 	public ModelAndView handleBadRequestException(InternalServerError ex, Model model) {
-		ModelAndView modelView = new ModelAndView("400");
+		ModelAndView modelView = new ModelAndView("error");
 		modelView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-		modelView.addObject("errorMessage", "Something went wrong. (500 Internal Server Error :-( ).");
+		modelView.addObject("errorMessage", "Something went wrong. ( 500 Internal Server Error :-( ).");
 		
 		return modelView;
 	}
 	
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ModelAndView handleBadRequestException(IllegalArgumentException ex, Model model) {
-		ModelAndView modelView = new ModelAndView("404");
+		ModelAndView modelView = new ModelAndView("error");
 		modelView.setStatus(HttpStatus.NOT_FOUND);
-		modelView.addObject("errorMessage", "Oops! Page Not Found(404 Not Found :-( ).");
+		modelView.addObject("errorMessage", "Oops! Page Not Found( 404 Not Found :-( ).");
 		
 		return modelView;
 	}
