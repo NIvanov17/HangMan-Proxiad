e45dac20250d4bba43d7133fd13a67a5817c5880
n.ivanov <n.ivanov@proxiad.com>
2024-09-19 16:13:25 +0300

    fix some integration tests

--- src/test/java/controller/MultiPlayerControllerIT.java
+++ src/test/java/controller/MultiPlayerControllerIT.java
@@ -1,55 +1,58 @@
 package controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import config.AppConfig;
+import config.RootConfig;
+import config.WebbInitialializer;
 import enums.Category;
-import jakarta.servlet.http.HttpSession;
 import model.Game;
 import model.History;
 import repository.WordsRepository;
 import service.GameService;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class,WebbInitialializer.class,RootConfig.class})
  class MultiPlayerControllerIT {
 
 	private MockMvc mockMvc;
-
-	@Mock
-	private GameService gameservice;
-
-	@Mock
-	private WordsRepository wordsRepository;
-
-	private Map<Game, History> mockHistory;
-
-	@InjectMocks
-	private MultiPlayerController controller;
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	GameService gameService;
+	
+	@Autowired
+	WordsRepository wordsRepository;
+	
+	Map<Game,History> injectedHistory;
 
 	@BeforeEach
 	 void setup() {
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-		mockHistory = wordsRepository.getHistory();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		injectedHistory = wordsRepository.getHistory();
 	}
 
 	@Test
@@ -72,7 +75,6 @@ import service.GameService;
 	@Test
 	 void multiPlayerSendInvalidWordIT() throws Exception {
 		String wordToGuess = "aab";
-		when(gameservice.isWordValid(wordToGuess)).thenReturn(false);
 		
 		mockMvc.perform(post("/multiPlayer")
 		.param("wordToGuess", wordToGuess)
@@ -83,33 +85,19 @@ import service.GameService;
 	
 	@Test
 	 void multiPlayerSendWordfromHistoryIT() throws Exception {
-		String wordToGuess = "test";
-		Game game = new Game();
-		game.setWord("test");
-		game.setCategory(Category.TECHNOLOGY);
-		
-		History history = new History();
-		history.setCategory(Category.TECHNOLOGY);
-		history.setFinished(false);
-		history.setMode("multiPlayer");
-		history.setTriesLeft(5);
-		history.setUsedChars(Set.of('t'));
-		history.setWordState("t _ _ t");
-		mockHistory.put(game, history);
-		lenient().when(gameservice.historyContainsWord(eq(mockHistory),eq(wordToGuess))).thenReturn(true);
-		
+		String wordToGuess = setUpHistory();
+
 		mockMvc.perform(post("/multiPlayer")
 		.param("wordToGuess", wordToGuess)
 		.param("action", ""))
 		.andExpect(status().is3xxRedirection())
 		.andExpect(redirectedUrl("/multiPlayer"));
 	}
+
 	
 	@Test
 	 void multiPlayerResumeIT() throws Exception {
-		String wordToGuess = "test";
-		when(gameservice.resumeGame(eq(wordToGuess), any(HttpSession.class), eq(mockHistory)))
-		.thenReturn("multiplayerStartedView");
+		String wordToGuess = setUpHistory();
 		
 		mockMvc.perform(post("/multiPlayer")
 		.param("currentWord", wordToGuess)
@@ -124,14 +112,6 @@ import service.GameService;
 		String wordToGuess = "testov";
 		Category category = Category.TECHNOLOGY;
 
-		when(gameservice.prepareWordToBeDisplayed(any(HttpSession.class),
-				eq(wordToGuess), 
-				eq(category)))
-		.thenReturn("multiplayerStartedView");
-		
-		when(gameservice.isWordValid(eq(wordToGuess)))
-		.thenReturn(true);
-	
 		mockMvc.perform(post("/multiPlayer")
 				.param("wordToGuess", wordToGuess)
 				.param("category", category.name()))
@@ -150,25 +130,54 @@ import service.GameService;
 
 	@Test 
 	 void multiPlayerGameStartedTryWrongIT() throws Exception {
+		mockMvc.perform(post("/multiplayerStarted").param("guess", "a").session(extractedSession()))
+		.andExpect(status().is3xxRedirection())
+		.andExpect(redirectedUrl("/multiplayerStarted"));
+	}
 
-		
-		when(gameservice.tryGuessMultiplayer(eq('a'), any(HttpSession.class)))
-		.thenReturn("redirect:/multiplayerStarted");
-
-		mockMvc.perform(post("/multiplayerStarted").param("guess", "a"))
+	
+	@Test 
+	 void multiPlayerGameStartedTryCorrectIT() throws Exception {
+		mockMvc.perform(post("/multiplayerStarted").param("guess", "e").session(extractedSession()))
 		.andExpect(status().is3xxRedirection())
 		.andExpect(redirectedUrl("/multiplayerStarted"));
 	}
 	
-	@Test 
-	 void multiPlayerGameStartedTryCorrectIT() throws Exception {
-
+	String setUpHistory() {
+		String wordToGuess = "test";
+		Game game = new Game();
+		game.setWord("test");
+		game.setCategory(Category.TECHNOLOGY);
 		
-		when(gameservice.tryGuessMultiplayer(eq('e'), any(HttpSession.class)))
-		.thenReturn("redirect:/multiplayerStarted");
-
-		mockMvc.perform(post("/multiplayerStarted").param("guess", "e"))
-		.andExpect(status().is3xxRedirection())
-		.andExpect(redirectedUrl("/multiplayerStarted"));
+		History history = new History();
+		history.setCategory(Category.TECHNOLOGY);
+		history.setFinished(false);
+		history.setMode("multiPlayer");
+		history.setTriesLeft(5);
+		history.setUsedChars(Set.of('t'));
+		history.setWordState("t _ _ t");
+		injectedHistory.put(game, history);
+		return wordToGuess;
+	}
+	
+	MockHttpSession extractedSession() {
+		MockHttpSession session = new MockHttpSession();
+		Set<Character> usedCharacters = new HashSet<>();
+		Game wordToFind = new Game();
+		wordToFind.setWord("test");
+		Category category = Category.TECHNOLOGY;
+		int triesLeft = 6;
+		String currentState = "t _ _ t"; 
+		boolean isFinished = false;
+		String mode = "Multiplayer";
+		
+		session.setAttribute("usedCharacters", usedCharacters);
+		session.setAttribute("word", wordToFind);
+		session.setAttribute("category", category);
+		session.setAttribute("triesLeft", triesLeft);
+		session.setAttribute("currentState", currentState);
+		session.setAttribute("isFinished", isFinished);
+		session.setAttribute("mode", mode);
+		return session;
 	}
 }
--- src/test/java/controller/SinglePlayerIT.java
+++ src/test/java/controller/SinglePlayerIT.java
@@ -11,7 +11,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,41 +21,50 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import config.AppConfig;
+import config.RootConfig;
+import config.WebbInitialializer;
+import enums.Category;
 import jakarta.servlet.http.HttpSession;
 import model.Game;
 import model.History;
 import repository.WordsRepository;
 import service.GameService;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class,WebbInitialializer.class,RootConfig.class})
  class SinglePlayerIT {
-
-	private MockMvc mockMvc;
-
-	@Mock
-	private GameService gameService;
-
-	@Mock
-	private WordsRepository wordsRepository;
-
-	@InjectMocks
-	private SinglePlayerController controller;
-
-	private Map<Game, History> mockHistory;
+private MockMvc mockMvc;
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	GameService gameService;
+	
+	@Autowired
+	WordsRepository wordsRepository;
+	
+	Map<Game,History> injectedHistory;
 
 	@BeforeEach
 	 void setup() {
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-		mockHistory = new HashMap<>();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		injectedHistory = wordsRepository.getHistory();
 	}
 
 	@Test
 	 void singlePlayerStartNewGame() throws Exception {
-		when(gameService.newGameStarted(any(HttpSession.class), eq(mockHistory))).thenReturn("gameStarted");
-
 		mockMvc.perform(get("/hangMan"))
 		.andExpect(status().isOk())
 		.andExpect(view().name("gameStarted"));
@@ -61,7 +72,7 @@ import service.GameService;
 
 	@Test
 	 void singlePlayerRestartGame() throws Exception {
-		mockMvc.perform(get("/hangMan").param("action", "restart"))
+		mockMvc.perform(get("/hangMan").param("action", "restart").session(extractedSession()))
 		.andExpect(status().is3xxRedirection())
 		.andExpect(redirectedUrl("/hangMan"));
 		
@@ -69,21 +80,64 @@ import service.GameService;
 
 	@Test
 	 void singlePlayerResumeGame() throws Exception {
-		when(gameService.resumeGame(eq("test"), any(HttpSession.class), eq(mockHistory))).thenReturn("gameStarted");
-
-		mockMvc.perform(get("/hangMan").param("action", "resume").param("currentWord", "test"))
+		String currentWord = setUpHistory();
+		
+		mockMvc.perform(get("/hangMan").param("action", "resume").param("currentWord", currentWord))
 		.andExpect(status().isOk())
 		.andExpect(view().name("gameStarted"));
 	}
 
 	@Test
 	 void singlePlayerTryGuess() throws Exception {
-		when(gameService.tryGuess(anyChar(), any(HttpSession.class), eq(mockHistory))).thenReturn("gameStarted");
-
-		mockMvc.perform(post("/hangMan").param("guess", "a"))
+		mockMvc.perform(post("/hangMan").param("guess", "e").session(extractedSession()))
 		.andExpect(status().isOk())
 		.andExpect(view().name("gameStarted"));
+	}
+	
+	@Test
+	 void singlePlayerTryWrongGuess() throws Exception {
+		mockMvc.perform(post("/hangMan").param("guess", "a").session(extractedSession()))
+		.andExpect(status().isOk())
+		.andExpect(view().name("gameStarted"));
+	}
+	
+	String setUpHistory() {
+		String wordToGuess = "test";
+		Game game = new Game();
+		game.setWord("test");
+		game.setCategory(Category.TECHNOLOGY);
 		
+		History history = new History();
+		history.setCategory(Category.TECHNOLOGY);
+		history.setFinished(false);
+		history.setMode("Single Player");
+		history.setTriesLeft(5);
+		history.setUsedChars(Set.of('t'));
+		history.setWordState("t _ _ t");
+		injectedHistory.put(game, history);
+		return wordToGuess;
+	}
+	
+	MockHttpSession extractedSession() {
+		MockHttpSession session = new MockHttpSession();
+		Set<Character> usedCharacters = new HashSet<>();
+		usedCharacters.add('t');
+		Game wordToFind = new Game();
+		wordToFind.setWord("test");
+		Category category = Category.TECHNOLOGY;
+		int triesLeft = 6;
+		String currentState = "t _ _ t"; 
+		boolean isFinished = false;
+		String mode = "SinglePlayer";
+		
+		session.setAttribute("usedCharacters", usedCharacters);
+		session.setAttribute("word", wordToFind);
+		session.setAttribute("category", category);
+		session.setAttribute("triesLeft", triesLeft);
+		session.setAttribute("currentState", currentState);
+		session.setAttribute("isFinished", isFinished);
+		session.setAttribute("mode", mode);
+		return session;
 	}
 
 }
